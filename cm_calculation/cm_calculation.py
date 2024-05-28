372167736acdfbd95a87c0522ee4e651463a1af5
Hamidreza Amirzadeh <h.amirzadeh1378@gmail.com>
2024-05-29 01:52:59 +0330

    added a script to save the cm scores as pandas dataframes

---
+++ cm_calculation/cm_calculation.py
@@ -0,0 +1,32 @@
+import cm_calculation.utils as utils
+import torch
+import os
+
+
+################# configs ################
+OUTPUT_DIR = "results/"
+if not os.path.exists(OUTPUT_DIR):
+  os.makedirs(OUTPUT_DIR)
+
+MODEL_CHECKPOINTS = ['roberta-base']
+DATASET_PATHS = ['data/gender_agreement.json']
+MAX_NUM_CUES = 10
+###########################################
+
+for model_ckpt in MODEL_CHECKPOINTS:
+  for dataset_path in DATASET_PATHS:
+    for num_cues in range(MAX_NUM_CUES):
+        torch.cuda.empty_cache()
+        d2ccm = utils.dataset2cuesCM(model_ckpt, dataset_path, num_cues)
+        df = d2ccm.retrieveCM()
+
+        # save some useful atributions for the dataframe
+        df.attrs["model_checkpoint"] = model_ckpt
+        df.attrs["dataset_path"] = dataset_path
+        df.attrs["num_cues"] = num_cues
+
+        # dump the dataframe to a pickle file
+        file_name = model_ckpt + '_' + dataset_path.split('/')[-1].split('.')[-2] + '_' + str(num_cues) + '.pkl'
+        df.to_pickle(OUTPUT_DIR + file_name)
+        print(OUTPUT_DIR + file_name)
+        print("=========================================")
