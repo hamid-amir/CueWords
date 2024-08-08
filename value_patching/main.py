939abfd114aebf2570babfc6542830b67070b8d5
Hamidreza Amirzadeh <h.amirzadeh1378@gmail.com>
2024-08-08 20:31:12 +0000

    added a script to save the vp scores as pandas dataframe

---
+++ value_patching/main.py
@@ -0,0 +1,34 @@
+import utils
+import torch
+import os
+
+
+################# configs ################
+OUTPUT_DIR = "results_vp/"
+if not os.path.exists(OUTPUT_DIR):
+  os.makedirs(OUTPUT_DIR)
+
+# MODEL_CHECKPOINTS = ['roberta-base', './finetuned-roberta', 'bert-base-uncased', './finetuned-bert', 'gpt2', './finetuned-gpt2', 'google/gemma-2b']
+MODEL_CHECKPOINTS = ['bert-base-uncased']
+DATASET_PATHS = ['data/gender_agreement']
+MIN_NUM_CUES = 2
+MAX_NUM_CUES = 6
+###########################################
+
+for model_ckpt in MODEL_CHECKPOINTS:
+  for dataset_path in DATASET_PATHS:
+    for num_cues in range(MIN_NUM_CUES, MAX_NUM_CUES+1):
+      torch.cuda.empty_cache()
+      d2vp = utils.dataset2VPs(model_ckpt, dataset_path, num_cues)
+      df = d2vp.retrieveVP()
+
+      # save some useful atributions for the dataframe
+      df.attrs["model_checkpoint"] = model_ckpt
+      df.attrs["dataset_path"] = dataset_path
+      df.attrs["num_cues"] = num_cues
+
+      # dump the dataframe to a pickle file
+      file_name = model_ckpt.split('/')[-1] + '_' + dataset_path.split('/')[-1] + '_' + str(num_cues) + '.pkl'
+      df.to_pickle(OUTPUT_DIR + file_name)
+      print(OUTPUT_DIR + file_name)
+      print("="*50)
